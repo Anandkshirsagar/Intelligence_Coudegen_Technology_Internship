05e4219d53415b93fb6e6eeca624e947abe62dbc
Anandkshirsagar <akshirsagarukd@gmail.com>
2026-03-16 20:26:19 +0530

    Week 5 Project add

--- Downloads/user-management-backend/user-management-backend/src/main/java/com/example/user_management_backend/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.user_management_backend.repository;
-
-import com.example.user_management_backend.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
